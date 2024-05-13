5fd9f9c32327797308315394826c0229dfc3aa14
anas-ce <anas@cyberevangelists.com>
2024-05-13 21:01:07 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,64 @@
+import re
+from PyPDF2 import PdfReader
+import json
+
+def extract_data_from_pdf(pdf_file):
+    with open(pdf_file, 'rb') as file:
+        reader = PdfReader(file)
+        report_date = None
+        data_tuples = []
+
+        for page in reader.pages:
+            text = page.extract_text()
+            lines = text.split('\n')
+            
+            for line in lines:
+             
+                if "Invoice Report by Contractor" in line:
+                    report_date_match = re.search(r'(\d{2}/\d{2}/\d{4})$', line)
+                    if report_date_match:
+                        report_date = report_date_match.group(1)
+                
+             
+                if "Assigned Owner" in line:
+                    assigned_owner_match = re.search(r'Assigned Owner (A\d+\s+.+)$', line)
+                    if assigned_owner_match:
+                        assigned_owner=(assigned_owner_match.group(1))
+                        
+              
+                cap_number_match = re.search(r'\bD\d+\s+', line)
+                if cap_number_match:
+                    cap_number = cap_number_match.group().strip()
+                    columns = line.split()
+                    if len(columns) >= 4:  # Check if line contains enough columns
+                        amount_requested = columns[1]
+                        received_date = columns[3]
+                        last_active_date = columns[2]
+                        data_tuples.append((cap_number, assigned_owner, received_date, last_active_date, report_date))
+        
+        return data_tuples
+
+def tuples_to_json(data_tuples):
+    json_data = []
+    for data_tuple in data_tuples:
+        json_entry = {
+            "D#": data_tuple[0],
+            "Status": data_tuple[1],
+            "Received Date": data_tuple[2],
+            "Last Active Date": data_tuple[3],
+            "Report Date": data_tuple[4]
+        }
+        json_data.append(json_entry)
+    return json_data
+
+
+pdf_file = 'invoice.pdf'
+data_tuples = extract_data_from_pdf(pdf_file)
+json_data = tuples_to_json(data_tuples)
+
+
+with open('data.json', 'w') as json_file:
+    json.dump(json_data, json_file, indent=4)
+
+
+print('Data transferred to data.json as tuples')
---
+++ readme.md
@@ -0,0 +1,40 @@
+# Invoice Data Extraction from PDF Readme
+
+## Introduction
+
+This script allows you to extract invoice data from a PDF file and convert it into a JSON format. It can be useful when you have multiple invoices in PDF format and you need to organize them into a structured format for further analysis or processing.
+
+## Requirements
+Before running the script, ensure you have the following installed:
+
+1. Python (version 3 or above)
+2. PyPDF2 library
+3. Regular Expression library (re)
+4. You can install PyPDF2 and re libraries using pip:
+
+
+```bash
+pip install -r requirements.txt
+```
+
+
+## Usage
+Download the Script: Save the provided Python script (invoice_extractor.py) to your local machine.
+
+
+## Run the Script:
+
+Open your terminal or command prompt.
+
+
+Navigate to the directory where the script invoice_extractor.py is saved.
+
+Run the script using the following command:
+
+```bash
+python main.py
+```
+
+Review Output: After running the script, it will create a file named data.json in the same directory. This JSON file contains the extracted invoice data in a structured format.
+
+
